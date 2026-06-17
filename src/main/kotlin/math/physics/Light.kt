cfff7339f47ecba0caba4064e000d33fe5f2a445
Blast12345 <germane.clipper.0q@icloud.com>
2026-06-17 01:28:59 -0500

    delete old code

--- src/main/kotlin/math/physics/Light.kt
+++ src/main/kotlin/math/physics/Light.kt
@@ -12,30 +12,6 @@ data class Light(
 
     val radiantFlux = red + green + blue
 
-//    val chromaticity: Chromaticity? by lazy {
-//        if (radiantFlux == 0.0) return@lazy null
-//
-//        val max = maxOf(red, green, blue)
-//        val min = minOf(red, green, blue)
-//        val chroma = max - min
-//
-//        if (chroma == 0.0) {
-//            return@lazy Chromaticity.Achromatic
-//        }
-//
-//        val hue = Angle.fromDegrees(
-//            when (max) {
-//                red -> 60.0 * ((green - blue) / chroma).mod(6.0)
-//                green -> 60.0 * ((blue - red) / chroma + 2.0)
-//                else -> 60.0 * ((red - green) / chroma + 4.0)
-//            }
-//        )
-//
-//        val saturation = UnitInterval.clamped(chroma / max)
-//
-//        return@lazy Chromaticity.Chromatic(hue, saturation)
-//    }
-
     operator fun plus(other: Light) = Light(
         red = red + other.red,
         green = green + other.green,
