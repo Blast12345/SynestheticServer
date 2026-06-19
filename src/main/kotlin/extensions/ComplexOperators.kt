6f5979f025a4a0f1eb2911db3193e14acc579a67
Blast12345 <germane.clipper.0q@icloud.com>
2026-06-18 22:18:48 -0500

    Removed unused complex operators

--- src/main/kotlin/extensions/ComplexOperators.kt
+++
@@ -1,9 +0,0 @@
-package extensions
-
-import org.apache.commons.math3.complex.Complex
-
-operator fun Complex.plus(other: Complex): Complex = add(other)
-operator fun Complex.minus(other: Complex): Complex = subtract(other)
-operator fun Complex.times(other: Complex): Complex = multiply(other)
-operator fun Complex.times(scalar: Double): Complex = multiply(scalar)
-operator fun Complex.div(scalar: Double): Complex = divide(scalar)
