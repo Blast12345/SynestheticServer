fc890d2a1513cc8deff63cef82718777e26a89e8
Blast12345 <germane.clipper.0q@icloud.com>
2026-06-20 13:15:39 -0500

    wip

--- src/test/kotlin/toolkit/monkeyTest/NextSpectralAnalysisConfig.kt
+++ src/test/kotlin/toolkit/monkeyTest/NextSpectralAnalysisConfig.kt
@@ -1,19 +1,26 @@
 package toolkit.monkeyTest
 
 import dsp.windowing.WindowType
+import lightOrgan.spectralAnalysis.AudioConditionerConfig
 import lightOrgan.spectralAnalysis.SpectralAnalysisConfig
 import kotlin.random.Random
 
 fun nextSpectralAnalysisConfig(): SpectralAnalysisConfig {
     return SpectralAnalysisConfig(
-        gainDb = nextPositiveFloat(),
+        audioConditioner = nextAudioConditionerConfig(),
         frameDuration = nextDuration(),
         approximateBinSpacing = nextPositiveFloat(),
-        rolloffThreshold = nextPositiveFloat(),
-        highPassFilter = nextHighPassConfig(),
-        lowPassFilter = nextLowPassConfig(),
         window = nextEnum<WindowType>(),
         peakExtractor = nextPeakExtractorConfig(),
+    )
+}
+
+fun nextAudioConditionerConfig(): AudioConditionerConfig {
+    return AudioConditionerConfig(
+        gainDb = nextPositiveFloat(),
+        highPassFilter = nextHighPassConfig(),
+        lowPassFilter = nextLowPassConfig(),
+        rolloffThreshold = nextPositiveFloat(),
         decimate = Random.nextBoolean()
     )
 }
